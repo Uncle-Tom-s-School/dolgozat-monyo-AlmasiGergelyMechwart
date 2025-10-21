3abb892ea4d7e2e91e9905adc0b8c9a09d0d10ed
AlmasiGergelyMechwart <22.almasi.gergely@mechwart.com>
2025-10-21 13:08:27 +0200

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import BeerCard from "./components/BeerCard"
+import { useState, useEffect } from "react"
+
+import BeerCard, { type BeerCardProps } from "./components/BeerCard"
 
 const App = () => {
+
+  const [data, setData] = useState<BeerCardProps[]>([])
+
+  useEffect(() => {
+    fetch("/data.json")
+    .then(resp => resp.json())
+    .then(dat => setData(dat))
+  }, [])
+
   return (
     <div className="beerCardWrapper">
-      <BeerCard />
+      { data.length>0 ?
+        data.map(beer => <BeerCard key={beer.name} {...beer} />)
+        :
+        <>
+          <p>Loading</p>
+        </>
+      }
     </div>
   )
 }
--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -1,6 +1,24 @@
-const BeerCard = () => {
+export type BeerCardProps = {
+  image: string,
+  name: string,
+  abv: string,
+  volume: string,
+  price: number,
+  available: boolean,
+};
+
+const BeerCard = (props: BeerCardProps) => {
   return (
-    <div className='beerCard'>Beer Card</div>
+    <div className='beerCard'>
+      <img src={props.image} />
+      <strong>{props.name} {props.abv} {props.volume}</strong>
+      <p>{props.price} Ft</p>
+      {props.available ?
+        <p className="available">Raktáron</p>
+        :
+        <p className="notAvailable">Nincs raktáron</p>
+      }
+    </div>
   )
 }
 
